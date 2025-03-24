0c52cedf11419a923665678aeaf265fd9b7a947d
SilemNabib <silemnabib@gmail.com>
2025-03-24 18:11:26 -0500

    feat: se combinaron los middlewares auth y authorization en uno solo

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,4 @@
-export * from "./auth.middleware";
-export * from "./validateSchema.middleware";
-export * from "./exception.middleware";
 export * from "./authorize.middleware";
+export * from "./bodyWrap.middleware";
+export * from "./exception.middleware";
+export * from "./validateSchema.middleware";
