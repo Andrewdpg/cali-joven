933225e3be1a91d8f86ce5994ef739a76d3297a3
SilemNabib <silemnabib@gmail.com>
2025-03-25 21:06:36 -0500

    chore: se eliminaron los try/catch especificos de la controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,92 +1,66 @@
 import { Request, Response } from "express";
+import { toCreationResponse, userMapper } from "../mappers";
+import { errorWrapper } from "../middleware";
 import { userService } from "../services";
 import { UserBase } from "../types";
-import { errorWrapper } from "../middleware";
-import { toCreationResponse, userMapper } from "../mappers";
 
 class UserController {
   // TODO: Agregar respuesta adecuada
   public async createUser(req: Request, res: Response) {
-    try {
-      const newUser = await userService.create(req.body.data as UserBase);
-      res.status(201).json(newUser);
-    } catch (error) {
-      if (error instanceof ReferenceError) {
-        res.status(400).json(error.message);
-        return;
-      }
-      res.status(500).json(error);
-    }
+    const newUser = await userService.create(req.body.data as UserBase);
+    res.status(201).json(newUser);
   }
 
   public getAllUsers(req: Request, res: Response) {
-    try {
-      return userService.findAllUsers().then((users) => {
-        res.status(200).json(users);
-      });
-    } catch (error) {
-      res.status(500).json(error);
-    }
+    return userService.findAllUsers().then((users) => {
+      res.status(200).json(users);
+    });
   }
 
   public getUserByEmail(req: Request, res: Response) {
-    try {
-      return userService.findUserByEmail(req.params.id).then((user) => {
-        if (!user) {
-          res.status(404).json(`User with id ${req.params.id} not found`);
-          return;
-        }
-        res.status(200).json(user);
-      });
-    } catch (error) {
-      res.status(500).json(error);
-    }
+    return userService.findUserByEmail(req.params.id).then((user) => {
+      if (!user) {
+        res.status(404).json(`User with id ${req.params.id} not found`);
+        return;
+      }
+      res.status(200).json(user);
+    });
   }
 
   public async updateUser(req: Request, res: Response) {
-    try {
-      return await userService
-        .updateUserById(req.params.id, req.body.data)
-        .then((user) => {
-          if (!user) {
-            res.status(404).json(`User with id ${req.params.id} not found`);
-            return;
-          }
-          res.status(200).json(user);
-        });
-    } catch (error) {
-      res.status(500).json(error);
-    }
+    return await userService
+      .updateUserById(req.params.id, req.body.data)
+      .then((user) => {
+        if (!user) {
+          res.status(404).json(`User with id ${req.params.id} not found`);
+          return;
+        }
+        res.status(200).json(user);
+      });
   }
 
   public async getUserById(req: Request, res: Response) {
-    try {
-      return await userService.findUserById(req.params.id).then((user) => {
-        if (!user) {
-          res.status(404).json(`User with id ${req.params.id} not found`);
-          return;
-        }
-        res.status(200).json(user);
-      });
-    } catch (error) {
-      res.status(500).json(error);
-    }
+    return await userService.findUserById(req.params.id).then((user) => {
+      if (!user) {
+        res.status(404).json(`User with id ${req.params.id} not found`);
+        return;
+      }
+      res.status(200).json(user);
+    });
   }
 
   public async deleteUser(req: Request, res: Response) {
-    try {
-      const authenticatedUserId = req.body.payload.user_id; // ID del usuario autenticado
+    const authenticatedUserId = req.body.payload.user_id; // ID del usuario autenticado
 
-      return await userService.deleteUserById(req.params.id, authenticatedUserId).then((user) => {
+    return await userService
+      .deleteUserById(req.params.id, authenticatedUserId)
+      .then((user) => {
         if (!user) {
           res.status(404).json(`User with id ${req.params.id} not found`);
           return;
         }
         res.status(200).json(user);
       });
-    } catch (error) {
-      res.status(500).json(error);
-    }
   }
 
   // Agregar un rol a un usuario
@@ -101,7 +75,9 @@ class UserController {
     async (req: Request, res: Response) => {
       const { id: userId, role } = req.params;
       const user = await userService.removeRoleFromUser(userId, role);
-      res.status(200).json(toCreationResponse(userMapper.DocumentToPublic(user)));
+      res
+        .status(200)
+        .json(toCreationResponse(userMapper.DocumentToPublic(user)));
     }
   );
 }
