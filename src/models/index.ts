e7f8d62a02139c473a77f290b30a5ba9c8c0d23a
Andrewdpg <andres.parra@u.icesi.edu.co>
2025-03-25 20:17:00 -0500

    feat: implementar gestión de usuarios en organizaciones con roles y permisos

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from "./user.model";
 export * from "./post.model";
 export * from "./attendee.model";
 export * from "./organization.model"
+export * from "./user_organization.model"
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -8,3 +8,8 @@ userRouter.get("/", authorize(["admin"]), userController.getAllUsers);
 userRouter.get("/:id", authorize(["admin"]), userController.getUserById);
 userRouter.put("/:id", authorize(["admin"]), userController.updateUser);
 userRouter.delete("/:id", authorize(["admin"]), userController.deleteUser);
+
+// Agregar un rol a un usuario
+userRouter.post("/:id/role/:role", authorize(["admin"]), userController.addRoleToUser);
+// Quitar un rol de un usuario
+userRouter.delete("/:id/role/:role", authorize(["admin"]), userController.removeRoleFromUser);
